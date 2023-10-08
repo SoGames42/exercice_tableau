de7aecef17cc42a55d969aca270f604fd8209595
Sofia <55157087+SoGames42@users.noreply.github.com>
2023-10-08 18:24:07 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let Names=[];
+
+(Names.push("Vincent"));
+(Names[0]);
+(Names.push("Paul"));
+(Names[1]);
+(Names.push("Arthur"));
+(Names[2]);
+
+for(let i=0;i<Names.length;i++){
+    console.log(`${Names[i]} va à la peche`);
+}
